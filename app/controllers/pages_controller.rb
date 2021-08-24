730c61cd804c5a5fb01e8405d57ac545d99facf1
nlmiv <napmanaog@aim.com>
2021-08-24 12:45:23 +1000

    newsletter functional

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def privacy
+    render :template => "pages/privacy.html.erb"
+  end
 end
--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,7 +1,32 @@
 class SubscribersController < ApplicationController
   before_action :check_for_login, :only => [:index]
+
+  def new
+    @subscriber = Subscriber.new
+  end
+
+  def show
+    # @subscriber = Subscriber.find params[:id]
+    # raise 'hell'
+  end
+
+  def create
+    subscriber = Subscriber.new subscriber_params
+    if subscriber.save
+      redirect_to subscriber_path subscriber
+    else
+      render :new
+    end
+  end
+
   def index
     @admins = Admin.all
     @subscribers = Subscriber.all
   end
+
+  private
+  def subscriber_params
+    params.require(:subscriber).permit(:first_name, :last_name, :email, :innovation, :botler_care, :updates, :special_deals )
+  end
+
 end
