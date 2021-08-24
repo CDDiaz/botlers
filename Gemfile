8531708c85a971e2f7076a7ed3fc16527e365c05
CDDiaz <cabj_xeneize@hotmail.com>
2021-08-24 12:39:57 +1000

    Table sorting added

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'bcrypt'
 gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'pry-rails'
+gem 'kaminari'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,8 +1,35 @@
 class SubscribersController < ApplicationController
   before_action :check_for_login, :only => [:index]
+  helper_method :sort_column, :sort_direction
+  
   def index
-    @subscribers = Subscriber.all
+
+    @subscribers = Subscriber
+      .page(params[:page]).per(10)
+      # .order(sort_by + " " + direction)
+      .order(sort_column + " " + sort_direction)
     @last_day_subs = Subscriber.all.where("DATE(created_at) = ?", Date.today - 1).count
     @total_subs = Subscriber.all.count
   end
+
+  private
+  # def define_order(attribute)
+  #   attribute.start_with?("-") ? :desc : :asc
+  #   sort_direction == "asc" ? "desc" : "asc"
+  # end
+  # Direction "created_at" is invalid. Valid directions are: [:asc, :desc, :ASC, :DESC, "asc", "desc", "ASC", "DESC"]
+  # def sort_by
+  #   %w{first_name email created_at}.include?(params[:sort_by]) ? params[:sort_by] : 'first_name'
+  # end
+  #
+  # def direction
+  #   %w{asc desc}.include?(params[:direction] == "asc") ? "desc" : "asc"
+  # end
+  def sort_column
+  Subscriber.column_names.include?(params[:sort]) ? params[:sort] : "first_name"
+  end
+  def sort_direction
+  %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"
+  end
+
 end
